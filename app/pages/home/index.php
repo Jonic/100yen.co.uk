9b02be874373b6ecfc1626a9826284c5d25f1dda
Jonic Linley <jonic@100yen.co.uk>
2020-05-27 21:04:10 +0100

    Markup for home page

--- app/pages/home/index.php
+++ app/pages/home/index.php
@@ -1,41 +1,42 @@
-<h1 class="c-type--page-title">I’m Jonic</h1>
+<?php
+$things = [
+    'I&nbsp;like to make things with code.',
+    'I&nbsp;sometimes make things out of electronics.',
+    'I’m&nbsp;a singer/guitarist/drummer/pianist.',
+    'I’m&nbsp;really good at Tetris.',
+    'I&nbsp;made a video game one time.',
+    '<span lang="ja">日本語を　勉強します。</span>',
+    'I&nbsp;have ADHD.',
+    'I’m&nbsp;named after Sonic the Hedgehog.',
+    'I’m&nbsp;from the UK.',
+    'I’m&nbsp;a Senior Software Engineer for <a class="c-link" href="https://www.raspberrypi.org">Raspberry Pi</a>.'
+];
+shuffle($things);
+?>
+
+<h1 class="c-type--page-title">Hiya. I’m Jonic.</h1>
 
 <p class="c-type--lead">
-    I like to make things with code.
-    I sometimes make things out of electronics.
-    I’m a singer/guitarist/drummer/pianist.
-    I’m really good at Tetris.
-    I made a video game one time.
-    I have ADHD.
-    I’m named after Sonic the Hedgehog.
-    I’m from the UK.
-    I’m a Senior Software Engineer for Raspberry Pi.
+    <?php echo join(' ', $things); ?>
 </p>
 
-<p>Find me around the web, if you like.</p>
+<p>
+    This site is a work-in-progress. More coming soon. Maybe.
+    Who knows. You saw the bit about me having ADHD.
+</p>
+<p>Find me around the web, if you like:</p>
 
 <ul>
-    <li><a href="https://twitter.com/Jonic">Twitter</a></li>
-    <li><a href="https://www.instagram.com/Jonic/">Instagram</a></li>
-    <li><a href="https://soundcloud.com/jonic">Soundcloud</a></li>
-    <li><a href="https://github.com/Jonic">Github</a></li>
+    <li><a class="c-link" href="https://twitter.com/Jonic">Twitter</a></li>
+    <li><a class="c-link" href="https://www.instagram.com/Jonic/">Instagram</a></li>
+    <li><a class="c-link" href="https://soundcloud.com/jonic">Soundcloud</a></li>
+    <li><a class="c-link" href="https://github.com/Jonic">Github</a></li>
 </ul>
 
-<!-- <div>
-    <h1>A Visual Type Scale</h1>
-    <p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
-    <p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
-    <h2>A Visual Type Scale</h2>
-    <p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
-    <p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
-    <h3>A Visual Type Scale</h3>
-    <p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
-    <p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
-    <h4>A Visual Type Scale</h4>
-    <p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
-    <p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
-    <h5>A Visual Type Scale</h5>
-    <p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
-    <p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
-    <p>— Excerpt from A Voyage to Arcturus, by David Lindsay.</p>
-</div> -->
+<p class="c-shoutout">
+    Thanks,
+    <a class="c-link" href="https://twitter.com/lexaloffle/status/1052920070316339201">Zep</a>
+</p>
+
+<div class="c-two-js"></div>
+<script src="/app/assets/vendor/two.js/two.min.js"></script>
