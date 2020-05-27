18e138cd07ed214dd9638946190f33c172a78042
Jonic Linley <jonic@100yen.co.uk>
2020-05-27 23:17:47 +0100

    LET’S DEBUG ON PROD

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 define('ENV', 'development');
 error_reporting(E_ALL);
 
-if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1') {
-    define('ENV', 'production');
-    error_reporting(0);
-}
+// if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1') {
+//     define('ENV', 'production');
+//     error_reporting(0);
+// }
 
 define('ROTORS_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
