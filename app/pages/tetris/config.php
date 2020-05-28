dbf98eda3cc7ff9cfb4f1ca6a7278dd7bfc586b9
Jonic Linley <jonic@100yen.co.uk>
2020-05-28 14:19:00 +0100

    Tetris og image and icon

--- app/pages/tetris/config.php
+++ app/pages/tetris/config.php
@@ -1,5 +1,7 @@
 <?php
 return [
+    'description' => 'Watch the blocks fall. You will be pleased.',
+    'cover_image' => '/app/pages/tetris/images/cover.png',
+    'icon' => '/app/pages/tetris/images/icon.png',
     'title' => 'Do, dododo, dododo...',
-    'description' => 'Watch the blocks fall. You will be pleased.'
 ];
