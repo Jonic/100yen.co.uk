ca6edc06dec1804fb3ca8819b9751cd98546b616
Jonic Linley <jonic@100yen.co.uk>
2020-05-30 00:52:25 +0100

    Stuff index

--- app/pages/stuff/index.php
+++ app/pages/stuff/index.php
@@ -3,5 +3,6 @@
 <p>Things to put in your eyes through your internet browser.</p>
 
 <ul>
-    <li><a class="c-link" href="./tetris">Tetris</a></li>
+    <li><a class="c-link" href="/stuff/10print">10 PRINT CHR$(205.5+RND(1)); : GOTO 10</a></li>
+    <li><a class="c-link" href="/stuff/tetris">Tetris</a></li>
 </ul>
